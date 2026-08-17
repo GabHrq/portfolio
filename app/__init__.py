7238dcd502301a17b1630bb55ddbb6b9c4c70057
Gabriel Henrique <gabriel.h.passos@outlook.com>
2026-08-17 18:45:05 -0300

    add .gitignore + ajeitar a implementação do i18n e rotas

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel
+
+from .config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-from app import routes
+def get_locale():
+    return request.accept_languages.best_match(
+        app.config['LANGUAGES']
+    )
+
+babel = Babel(app, locale_selector=get_locale)
+from . import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
-from app import app
+from . import app
 
-@app.route("/")
 @app.route("/index")
+@app.route("/")
 def index():
     return render_template('index.html', title='Início')
